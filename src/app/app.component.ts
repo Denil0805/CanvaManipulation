0290ad3d17db671d2dbbd60fa08f421f2af1093b
GunanithiMurugesan <138852458+GunanithiMurugesan@users.noreply.github.com>
2024-03-05 12:10:35 +0530

    Package installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {fabric} from 'fabric';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'canvaManipulation';
+  ngAfterViewInit() {
+    this.canvaManipulation();
+  }
+  canvaManipulation() {
+    var canvas = new fabric.Canvas('canvas');
+    var rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 20,
+      height: 20
+    });
+    canvas.add(rect);
+  }
 }
